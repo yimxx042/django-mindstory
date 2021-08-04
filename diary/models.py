a502354017d65fd266f35fb585635da141a41e3b
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-04 11:03:51 -0500

    modeling

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Page(models.Model):
+    #title, content, feeling, score, dt_created
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    feeling = models.CharField(max_length=80)
+    score = models.IntegerField()
+    dt_created = models.DateField()
+
+    def __str__(self):
+        return self.title
+
