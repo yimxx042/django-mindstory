79ff6024a22d38eaf206994e83de20d0e22e83e4
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-04 13:20:35 -0500

    add detail pages

--- diary/views.py
+++ diary/views.py
@@ -6,3 +6,7 @@ from .models import Page
 def page_list(request):
     object_list = Page.objects.all()
     return render(request, 'diary/page_list.html', {'object_list': object_list})
+
+def page_detail(request, page_id):
+    object = Page.objects.get(id=page_id)
+    return render(request, 'diary/page_detail.html', {'object': object})
