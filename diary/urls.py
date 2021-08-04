c31d74e2a3446a59fab3a59357eefbf2edcceff3
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-04 10:34:51 -0500

    set URL for diary

---
+++ diary/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    #path('diary/', views.page_list),
+    #path('diafy/info/', views.info),
+    #path('diary/write/', views.page_create),
+    #path('diary/page/<int:page_id>/', views.page_detail),
+    #path('diary/page/<int:page_id>/edit/', views.page_update),
+    #path('diary/page/<int:page_id>/delete/', views.page_delete),
+]
