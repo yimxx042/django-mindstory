91b7aeb322c5fc055e184dd94130f2d85ccce8d6
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-04 15:37:48 -0500

    add form

--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Page
 from .forms import PageForm
 
@@ -16,5 +16,16 @@ def info(request):
     return render(request, 'diary/info.html')
 
 def page_create(request):
-    form = PageForm()
-    return render(request, 'diary/page_form.html', {'form': form})
+    if request.method == 'POST': 
+        new_page = Page( #post these data
+            title=request.POST['title'],  
+            content=request.POST['content'],
+            feeling=request.POST['feeling'],
+            score=request.POST['score'],
+            dt_created=request.POST['dt_created']
+        )
+        new_page.save() # save on database
+        return redirect('page-detail', page_id=new_page.id)
+    else:
+        form = PageForm() #new empty form
+        return render(request, 'diary/page_form.html', {'form': form})
