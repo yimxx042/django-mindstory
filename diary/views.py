532dce04cbe311bd8286354db03f75e41fc27734
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-05 14:05:21 -0500

    replace to generic view

--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from django.views.generic import CreateView
+from django.urls import reverse
 from .models import Page
 from .forms import PageForm
-from django.core.paginator import Paginator
+
 
 # Create your views here.
 
@@ -21,23 +24,31 @@ def page_detail(request, page_id):
 def info(request):
     return render(request, 'diary/info.html')
 
-def page_create(request):
-    if request.method == 'POST': 
-        form = PageForm(request.POST) # make binding form the inserted value
-        if form.is_valid(): #only when the data is validated
-            new_page = form.save()  #data save and return saved data
-        # new_page = Page( #post these data
-        #     title=request.POST['title'],  
-        #     content=request.POST['content'],
-        #     feeling=request.POST['feeling'],
-        #     score=request.POST['score'],
-        #     dt_created=request.POST['dt_created']
-        # )
-        # new_page.save() # save on database
-            return redirect('page-detail', page_id=new_page.id) #redirect to new page
-    else:
-        form = PageForm() #new empty form
-    return render(request, 'diary/page_form.html', {'form': form})
+class PageCreateView(CreateView):
+    model = Page
+    form_class = PageForm
+    template_name = 'diary/page_form.html'
+
+    def get_success_url(self):
+        return reverse('page-detail', kwargs={'page_id': self.object.id})
+
+# def page_create(request):
+#     if request.method == 'POST': 
+#         form = PageForm(request.POST) # make binding form the inserted value
+#         if form.is_valid(): #only when the data is validated
+#             new_page = form.save()  #data save and return saved data
+#         # new_page = Page( #post these data
+#         #     title=request.POST['title'],  
+#         #     content=request.POST['content'],
+#         #     feeling=request.POST['feeling'],
+#         #     score=request.POST['score'],
+#         #     dt_created=request.POST['dt_created']
+#         # )
+#         # new_page.save() # save on database
+#             return redirect('page-detail', page_id=new_page.id) #redirect to new page
+#     else:
+#         form = PageForm() #new empty form
+#     return render(request, 'diary/page_form.html', {'form': form})
 
 
 def page_update(request, page_id):
